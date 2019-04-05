7b1974b445129c380f6c5e021f0fcbc3a3dc319b
PiotrWR <pwrynio@gmail.com>
2019-04-05 16:07:48 +0200

    Added 'Matomo' (statistic)tracking script.

---
+++ js/outerScripts/trackingMatomo.js
@@ -0,0 +1,19 @@
+// <!-- Matomo -->
+var _paq = _paq || [];
+/* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+_paq.push(['trackPageView']);
+_paq.push(['enableLinkTracking']);
+(function () {
+  var u = "//stats38.mydevil.net/";
+  _paq.push(['setTrackerUrl', u + 'piwik.php']);
+  _paq.push(['setSiteId', '9']);
+  var d = document,
+    g = d.createElement('script'),
+    s = d.getElementsByTagName('script')[0];
+  g.type = 'text/javascript';
+  g.async = true;
+  g.defer = true;
+  g.src = u + 'piwik.js';
+  s.parentNode.insertBefore(g, s);
+})();
+// <!-- End Matomo Code -->
--- js/script.js
+++ js/script.js
@@ -2,3 +2,4 @@ $.getScript("js/preloader.js", function () {});
 $.getScript("js/stickyNavBar.js", function () {});
 $.getScript("js/navFile.js", function () {});
 $.getScript("js/cookiesScript.js", function () {});
+$.getScript("js/outerScripts/trackingMatomo.js", function () {});
